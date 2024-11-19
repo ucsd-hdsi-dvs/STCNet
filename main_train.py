19f9b8fa35db1a4c646b3a0abf82b7056f2701df
j8chiu <j8qiu@ucsd.edu>
2024-11-19 05:05:09 +0000

    d

--- main_train.py
+++ main_train.py
@@ -140,8 +140,8 @@ def main():
 
             optimizer.step()
             epoch_loss += loss.item()
-            if iteration == 12:
-                break
+            # if iteration == 12:
+            #     break
 
 
         #### Evaluation ####
@@ -170,8 +170,8 @@ def main():
 
                     psnr_rgb = PSNR(input1, input2)
                     psnr_val_rgb.append(psnr_rgb)
-                if ii == 12:
-                    break
+                # if ii == 12:
+                #     break
 
             ssim_val_rgb = np.mean(ssim_val_rgb)
             psnr_val_rgb = np.mean(psnr_val_rgb)
